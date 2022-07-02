a5137939f9e5a49956e051cfe47d9af3469abe6a
someonenee <106909150+someonenee@users.noreply.github.com>
2022-07-02 14:49:02 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,124 @@
+
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+    /**
+   * Navbar links active state on scroll
+   */
+     let navbarlinks = document.querySelectorAll('#navbar a');
+
+     function navbarlinksActive() {
+       navbarlinks.forEach(navbarlink => {
+   
+         if (!navbarlink.hash) return;
+   
+         let section = document.querySelector(navbarlink.hash);
+         if (!section) return;
+   
+         let position = window.scrollY + 200;
+   
+         if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+           navbarlink.classList.add('active');
+         } else {
+           navbarlink.classList.remove('active');
+         }
+       })
+     }
+     window.addEventListener('load', navbarlinksActive);
+     document.addEventListener('scroll', navbarlinksActive);
+
+
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+ 
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()
